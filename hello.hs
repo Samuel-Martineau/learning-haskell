4c81ea396cf25d271aacac80631f5f5b9a71027d
Samuel Martineau <samuelmartineau@ophelia.taile2b48.ts.net>
2024-06-24 19:14:23 -0400

    My function

--- hello.hs
+++ hello.hs
@@ -1,6 +1,11 @@
+pow :: Integer -> Integer -> Integer
+pow a b = b ^ a
+
+square = pow 2
+
 main :: IO ()
 main = do
   putStrLn "Hello, everybody!"
   putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20] :: [Integer]))
   putStrLn ("And my favorite even numbers: " ++ show (take 10 (filter even [854..]) :: [Integer]))
-  
+  putStrLn ("The square of 5 is " ++ show (square 5))
