3cc92f38cf55edf1587baea8c3e13994d5e4e1a4
Samuel Martineau <samuelmartineau@ophelia.taile2b48.ts.net>
2024-06-24 19:08:55 -0400

    Fix some compilation warnings

--- hello.hs
+++ hello.hs
@@ -1,3 +1,6 @@
+main :: IO ()
 main = do
   putStrLn "Hello, everybody!"
-  putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20]))
+  putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20] :: [Integer]))
+  putStrLn ("And my favorite even numbers: " ++ show (take 10 (filter even [854..]) :: [Integer]))
+  
